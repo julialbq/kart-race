972a8bf9784be0140870fda56978e058f76eebd3
julia <julia.albq@gmail.com>
2023-02-26 14:51:24 -0300

    refactor: add components to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PilotForm } from "./components/PilotForm/PilotForm";
+import { Pilots } from "./components/Pilots/Pilots";
+import { TournamentForm } from "./components/TournamentForm/TournamentForm";
+import { Tournaments } from "./components/Tournaments/Tournaments";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PilotForm />
+      <Pilots />
+      <TournamentForm />
+      <Tournaments />
     </div>
   );
 }
